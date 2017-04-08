ec2df432f28779e4fdf609199cf12164500b1126
marleysidapple <bsiddhartha25@gmail.com>
2017-04-08 10:14:53 +0800

    added path

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 let app = express();
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './../public/index.html'));
 });
 
 
