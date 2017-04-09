5f9d3cc759eec981d3bfeaac27e49dd7142c9a03
marleysidapple <bsiddhartha25@gmail.com>
2017-04-09 16:09:52 +0800

    user signup

--- server/app/Http/Controllers/Api/V1/SignupController.php
+++ server/app/Http/Controllers/Api/V1/SignupController.php
@@ -14,12 +14,20 @@ class SignupController extends Controller
     {
     	DB::BeginTransaction();
     	try {
-    		$user = User::create($request->all());
+    		$user = User::create(array(
+    				'fullname'	=> $request->fullname,
+    				'email'		=> $request->email,
+    				'mobile'	=> $request->mobile,
+    				'address'	=> $request->address,
+    				'password'	=> \bcrypt($request->password)
+    			));
+
+    		//$user = User::create($request->all());
 			DB::commit();
     	} catch (\Exception $e) {
     		   DB::rollback();
     		   return response()->json('something went wrong', 400);
     	}
-    	return response()->json('status is ok', 200);
+    	return response()->json('user registered successfully', 200);
     }
 }
