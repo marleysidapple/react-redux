658de3383f7bb74ac8786d42719f8061e73de190
marleysidapple <bsiddhartha25@gmail.com>
2017-04-14 11:41:07 +0800

    added dispatcher

--- src/actions/SignupActions.js
+++ src/actions/SignupActions.js
@@ -4,7 +4,15 @@ import axios from 'axios';
 const ROOT_URL = 'http://localhost:8000/api/v1';
 
 export function userSignupRequest(userData){
-	return dispatch => {
-		axios.post(ROOT_URL + '/auth/register', userData);
+	const url  = `${ROOT_URL}/auth/register`;
+	const request = axios.post(url, userData);
+
+	// return dispatch => {
+	// 	axios.post(url, userData);
+	// }
+	return {
+		type: 'SAVE_USER',
+		payload: request
 	}
+
 }
--- src/components/modules/Signup.js
+++ src/components/modules/Signup.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import './../../App.css';
-import Signupform from './Signup-form';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { userSignupRequest } from '../../actions/SignupActions';
 
 class Signup extends Component {
+
+
+	 constructor(props) {
+	    super(props);
+	    this.state = {
+	    	fullname: '', 
+	    	email: '',
+	    	address: '',
+	    	mobile: '', 
+	    	password: '',
+	    	confirm_password: '' 
+		};
+	  }
+
+
+	handleChange(event){
+		this.setState({[event.target.name]: event.target.value});
+	}
+
+	handleChangeEmail(event){
+		this.setState({email: event.target.value});
+	}
+
+	handleSubmit(event) {
+	 event.preventDefault();
+     this.props.userSignupRequest(this.state);
+     //axios.post(this.base_url + '/auth/register', {user: this.state});
+   }
+
 
 	render(){
 
@@ -14,17 +43,53 @@ class Signup extends Component {
 			<div className="panel panel-default registration">
 				  <div className="panel-heading">User Registration</div>
 				  	<div className="panel-body">
-						<Signupform userSignupRequest={userSignupRequest} />
+						<form onSubmit = {this.handleSubmit.bind(this)}>
+					  	  <div className="form-group">
+						    <label>Full Name</label>
+						    <input type="text" className="form-control" name="fullname" placeholder="Full Name" value={this.state.fullname} onChange={this.handleChange.bind(this)}/>
+						  </div>
+
+						   <div className="form-group">
+						    <label>Email</label>
+						    <input type="email" className="form-control" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange.bind(this)}/>
+						  </div>
+
+						   <div className="form-group">
+						    <label>Address</label>
+						    <input type="text" className="form-control" name="address" placeholder="Address" value={this.state.address} onChange={this.handleChange.bind(this)}/>
+						  </div>
+
+						   <div className="form-group">
+						    <label>Mobile</label>
+						    <input type="text" className="form-control" name="mobile" placeholder="Mobile" value={this.state.mobile} onChange={this.handleChange.bind(this)}/>
+						  </div>
+
+						   <div className="form-group">
+						    <label>Password</label>
+						    <input type="password" className="form-control" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange.bind(this)}/>
+						  </div>
+
+						   <div className="form-group">
+						    <label>Confirm Password</label>
+						    <input type="password" className="form-control" name="confirm_password" placeholder="Confirm password" value={this.state.confirm_password} onChange={this.handleChange.bind(this)}/>
+						  </div>
+						  <button type="submit" className="btn btn-default">Submit</button>
+					  </form>
 					</div>
 			</div>
 		);
 	}
 }
 
-Signup.propTypes = {
-	userSignupRequest: React.PropTypes.func.isRequired
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({userSignupRequest}, dispatch);
 }
 
 
-//export default connect((state) => { return {} }, { userSignupRequest })(Signup);
-export default connect(null, { userSignupRequest })(Signup);
+// Signup.propTypes = {
+// 	userSignupRequest: React.PropTypes.func.isRequired
+// }
+
+
+export default connect(null, mapDispatchToProps)(Signup);
