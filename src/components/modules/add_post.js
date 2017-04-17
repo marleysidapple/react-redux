82b6dbf2d3dea68036e1149e8b79cd8b47dbdd02
marleysidapple <bsiddhartha25@gmail.com>
2017-04-17 16:49:03 +0800

    added validation

--- src/components/modules/add_post.js
+++ src/components/modules/add_post.js
@@ -4,20 +4,34 @@ import './css/post.css';
 import { createPost } from './../../actions/index';
 
 
-
-
 class AddPost extends Component {
-	
+
+
+
 	render(){
-		const { fields: {title}, handleSubmit } = this.props;
+	const renderField = ({ input, label, type, meta: { touched, error, invalid, warning } }) => (
+	 <div className={`form-group ${touched && invalid ? 'has-error' : ''}`}>
+	    <label>{label}</label>
+	    <div>
+	      <input {...input} placeholder={label} type={type} className="form-control"/>
+	      {touched && ((error && <span className="help-block">{error}</span>) || (warning && <span className="help-block">{warning}</span>))}
+	    </div>
+	  </div>
+	)
+
+		const { fields: {title, author, keyword, description }, handleSubmit,  pristine, reset, submitting} = this.props;
 		return (
 				<div className="post-wrap">
 					<form onSubmit={handleSubmit}>
+						<Field name="title" type="text" component={renderField} label="Title"/>
+						<Field name="author" type="text" component={renderField} label="Author"/>
+						<Field name="keyword" type="text" component={renderField} label="Keyword"/>
+
 						<div className="form-group">
-							<label>Title</label>
-							<Field className="form-control" component="input" type="text" name="title" {...title}  />
+							<label>Description</label>
+							<textarea className="form-control" {...description}/>
 						</div>
-						<button className="btn btn-primary btn-sm">Submit</button>
+						<button className="btn btn-primary btn-sm" disabled={submitting}>Submit</button>
 					</form>
 				</div>
 			);
@@ -25,21 +39,30 @@ class AddPost extends Component {
 }
 
 
+function validate(vals){
+	const errors = {};
 
+	if (!vals.title){
+		errors.title = 'Title is required';
+	}
+	if (!vals.keyword){
+		errors.keyword = 'Keyword is required';
+	}
+	if (!vals.author){
+		errors.author = 'Author is required';
+	}
+	return errors;
+
+}
 
 //connect: first argument is mapStatetoProps. second is mapDispatchToProps
 //reduxForm: first - formConfig, second - mapStateToProps, third - mapDispatchToProps 
-
-
-
 export default reduxForm({
 	form: 'AddNewPost',  //donot need to match with the class name
-	//fields: ['title', 'author', 'keyword', 'description']
-	fields: ['title'],
-	onSubmit: (post) => createPost(post)
+	validate,
+	fields: ['title', 'author', 'keyword', 'description'],
+	onSubmit: (post) => createPost(post),
 })(AddPost);
 
 
 
-
-//export default connect(null, {createPost})(reduxForm(formData)(AddPost));
---
+++ src/components/modules/add_post_backup.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import './css/post.css';
+import { createPost } from './../../actions/index';
+
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+	  <div>
+	    <label>{label}</label>
+	    <div className="form-group">
+	      <input {...input} placeholder={label} type={type} className="form-control"/>
+	      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+	    </div>
+	  </div>
+	);
+
+
+class AddPost extends Component {
+
+	render(){
+		const { fields: {title}, handleSubmit, touched, valid} = this.props;
+		return (
+				<div className="post-wrap">
+					<form onSubmit={handleSubmit}>
+						<Field component={renderField} name="title" label="Title" type="text"/>
+						<button className="btn btn-primary btn-sm">Submit</button>
+					</form>
+				</div>
+			);
+	}
+}
+
+function validate(formProps) {
+  const errors = {};
+
+  if (!formProps.title) {
+    errors.firstName = 'Please enter a first name';
+  }
+  return errors;
+}
+
+
+//connect: first argument is mapStatetoProps. second is mapDispatchToProps
+//reduxForm: first - formConfig, second - mapStateToProps, third - mapDispatchToProps 
+
+
+
+export default reduxForm({
+	form: 'AddNewPost',  //donot need to match with the class name
+	validate,
+	//fields: ['title', 'author', 'keyword', 'description']
+	fields: ['title'],
+	onSubmit: (post) => createPost(post),
+})(AddPost);
