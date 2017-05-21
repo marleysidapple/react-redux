8c69c734dbe5b6a2a9a0d75b2a6b8d89184418f2
marleysidapple <bsiddhartha25@gmail.com>
2017-05-21 11:08:29 +0800

    fix

--- src/components/modules/postedit.js
+++ src/components/modules/postedit.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 import { fetchPostById } from './../../actions/index';
+
+const renderField = ({ input, label, type, meta: { touched, error, invalid, warning } }) => (
+	 <div className={`form-group ${touched && invalid ? 'has-error' : ''}`}>
+	    <label>{label}</label>
+	    <div>
+	      <input {...input} placeholder={label} type={type} className="form-control"/>
+	      {touched && ((error && <span className="help-block">{error}</span>) || (warning && <span className="help-block">{warning}</span>))}
+	    </div>
+	  </div>
+	)
+
+	const renderFieldForTextArea = ({ input, label, type, meta: { touched, error, invalid, warning } }) => (
+	 <div className={`form-group ${touched && invalid ? 'has-error' : ''}`}>
+	    <label>{label}</label>
+	    <div>
+	      <textarea {...input} placeholder={label} type={type} className="form-control"/>
+	      {touched && ((error && <span className="help-block">{error}</span>) || (warning && <span className="help-block">{warning}</span>))}
+	    </div>
+	  </div>
+	)
 
 class Postedit extends Component {
 
@@ -8,13 +29,18 @@ class Postedit extends Component {
 		super(props);
 	}
 
-	componentDidMount(){
+	componentWillMount(){
 		this.props.fetchPostById(this.props.match.params.id);
+	}
+
+	handleSubmit(){
+
 	}
 
 
 	render(){
 
+		const { fields: {title, author, keyword, description }, handleSubmit, pristine, reset, submitting} = this.props;
 		const { post } = this.props;
 
 		if (!post) {
@@ -23,9 +49,13 @@ class Postedit extends Component {
 
 		return (
 			<div>
-				{post.author}
-				{post.title}
-				{post.description}
+				<form>
+					<Field name="title" type="text" value={post.title} component={renderField} label="Title"/>
+					<Field name="author" type="text"  component={renderField} label="Author"/>
+					<Field name="keyword" type="text"  component={renderField} label="Keyword"/>
+					<Field name="description" type="text"  component={renderFieldForTextArea} label="Description"/>
+					<button className="btn btn-primary btn-sm"  disabled={pristine || submitting}>Update</button>&nbsp; &nbsp;
+				</form>
 			</div>
 
 			);
@@ -35,10 +65,21 @@ class Postedit extends Component {
 
 function mapStateToProps(state){
 	return {
-		post: state.posts.post
+		post: state.posts.post,
+		initialValues: state.posts.post //specially to load initial values in form for edit purpose
 	}
 }
 
+function validate(){
+
+}
 
 
-export default connect(mapStateToProps, {fetchPostById})(Postedit);
+export default connect(mapStateToProps, {fetchPostById})(reduxForm({
+  form:'AddNewPost',
+  fields: ['title', 'author', 'keyword', 'description'],
+  validate
+})(Postedit));
+
+
+//export default connect(mapStateToProps, {fetchPostById})(Postedit);
